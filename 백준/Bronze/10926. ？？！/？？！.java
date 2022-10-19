e09577037928a4103d4d81b8dbf06a8a7433ac16
Moly <102847513+jminkkk@users.noreply.github.com>
2022-10-19 16:11:18 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String a = scanner.next();
+        scanner.close();
+        System.out.println(a+"??!");
+    }
+}
