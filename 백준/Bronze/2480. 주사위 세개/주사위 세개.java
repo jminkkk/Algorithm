0eb2663736f506c7cd6d557ae392a8b71b129bd4
Moly <102847513+jminkkk@users.noreply.github.com>
2022-10-25 16:04:13 +0900

    [Bronze IV] Title: 주사위 세개, Time: 200 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        scanner.close();
+
+        if((a==b) && (b==c)) System.out.println(10000+a*1000);
+        else if ((a == b)&& (a != c)) System.out.println(1000+a*100);
+        else if ((a == c)&& (a != b)) System.out.println(1000+a*100);
+        else if ((c == b)&& (a != c)) System.out.println(1000+c*100);
+        else {
+            if(a>b && a>c) System.out.println(a*100);
+            else if (b>a && b>c) System.out.println(b*100);
+            else System.out.println(c*100);
+        }
+    }
+}
