93dd469e9d99f96327d31609f1d15537a82653b9
Moly <102847513+jminkkk@users.noreply.github.com>
2022-10-30 13:18:29 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i<N+1 ; i++){
+            System.out.println("*".repeat(i));
+        }
+    }
+}
