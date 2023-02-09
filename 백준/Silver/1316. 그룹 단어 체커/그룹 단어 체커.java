97c08280746e6df8e5e383a43cb5dffd6f0925bd
Moly <102847513+jminkkk@users.noreply.github.com>
2023-02-10 01:07:26 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());    // 단어의 개수
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            if(checkGroup(str) == true) count++;
+        }
+
+        System.out.println(count);
+    }
+
+    private static Boolean checkGroup(String str) {
+        boolean[] check = new boolean[26]; // 한번 나오면 true
+        int prev = 0;
+
+        for(int j =0; j<str.length();j++){
+            int now = str.charAt(j);
+
+            if(prev != now)
+                if(check[now -'a'] == false) {// 해당 문자가 처음 나오는 경우 (false 인 경우)
+                    check[now - 'a'] = true;
+                    prev = now;
+                }
+                else return false;
+        }
+        return true;
+    }
+}
