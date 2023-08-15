8db40e0552d9f5dae0326d422fcc7b67bcc21139
Moly <102847513+jminkkk@users.noreply.github.com>
2023-08-15 17:30:06 +0900

    [Silver I] Title: 안정적인 문자열, Time: 172 ms, Memory: 17000 KB -BaekjoonHub

--- 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.java
+++ 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.java
@@ -1,50 +1,42 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Integer> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        String str = br.readLine();
+        int index = 1;
+        int count;
 
         while (true) {
-            String str = br.readLine();
-            if (str.indexOf('-') != -1) {
-                break;
-            }
+            if (str.charAt(0) == '-') break;
+            count = 0;
 
-            int num = checkNum(str);
-            list.add(num);
-        }
+            Queue<Character> queue = new LinkedList();
 
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(i + 1 + ". " + list.get(i));
-        }
-    }
-
-    // 안정된 문자열 만들기 위한 최소 연산의 수
-    public static Integer checkNum(String str) {
-        int count = 0;
-        Stack stack = new Stack<>();
-
-        if (str == null || str.length() == 0) {
-            return 0;
-        }
-
-        char[] arr = str.toCharArray();
-
-        for (char c : arr) {
-            if (c == '{') stack.add('{');
-            else {
-                if (stack.empty()) {
-                    count++;
-                    stack.add('{');
+            for (Character c : str.toCharArray()) {
+                if (c == '{') {
+                    queue.add(c);
+                } else {
+                    if (queue.isEmpty()) {
+                        count++;
+                        queue.add(c);
+                    } else queue.remove();
                 }
-                else stack.pop();
             }
+
+            if (!queue.isEmpty()) count += queue.size() / 2;
+            sb.append(index + ". " + count + "\n");
+
+            str = br.readLine();
+            index++;
         }
-        return count + stack.size()/2;
+
+        System.out.println(sb.toString());
     }
 }
