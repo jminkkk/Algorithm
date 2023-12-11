893c46e9c5f8b9998f7fcc79758c4745456ba442
Moly <102847513+jminkkk@users.noreply.github.com>
2023-12-11 21:36:24 +0900

    [Silver III] Title: 카드 놓기, Time: 816 ms, Memory: 149284 KB -BaekjoonHub

--- 백준/Silver/18115. 카드 놓기/카드 놓기.java
+++ 백준/Silver/18115. 카드 놓기/카드 놓기.java
@@ -1,42 +1,45 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         String[] arr = br.readLine().split(" ");
-
         int[] answer = new int[N];
+        
         int index1 = 0;
         int index2 = 1;
         int index3 = N - 1;
-
+        // N에 1번째거를 한거
         for (int i = N; i > 0; i--) {
-            int arrIndex = N - i;
-
-            if (arr[arrIndex].equals("1")) {
+            int index = N - i;
+            if (arr[index].equals("1")) {
                 answer[index1] = i;
+                // 비어있어야함
                 if (index1 + 1 < N && answer[index1 + 1] == 0) {
                     index1++;
+                } else {
+                    index1 = index2 + 1;
                 }
-                else index1 = index2 + 1;;
-            } else if (arr[arrIndex].equals("2")) {
+            } else if (arr[index].equals("2")) {
                 if (answer[index1 + 1] == 0) {
                     index2 = index1 + 1;
-                } else index2 += 1;
+                } else {
+                    index2 += 1;
+                }
                 answer[index2] = i;
-            } else if (arr[arrIndex].equals("3")) {
+            } else {
                 answer[index3--] = i;
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i : answer) {
+
+        for (int i: answer) {
             sb.append(i + " ");
         }
-        System.out.println(sb);
+
+        System.out.print(sb.toString());
     }
 }
