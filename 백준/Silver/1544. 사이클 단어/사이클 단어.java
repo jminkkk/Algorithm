87892fe2e3cc61e9034a9c067d6f5befbd91ecd4
Moly <102847513+jminkkk@users.noreply.github.com>
2023-12-24 21:23:35 +0900

    [Silver IV] Title: 사이클 단어, Time: 140 ms, Memory: 14816 KB -BaekjoonHub

--- 백준/Silver/1544. 사이클 단어/사이클 단어.java
+++ 백준/Silver/1544. 사이클 단어/사이클 단어.java
@@ -1,36 +1,28 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        ArrayList<String>[] arr = new ArrayList[N];
-
+        String[] arr = new String[N];
+        List<String> list = new ArrayList<>();
         int count = 0;
 
         for (int i = 0; i < N; i++) {
-            arr[i] = new ArrayList<>();
-            String cur = br.readLine();
+            arr[i] = br.readLine();
 
-            for (int j = 0; j < cur.length(); j++) {
-                arr[i].add(cur.substring(j) + cur.substring(0, j));
-            }
-
-            boolean chk = true;
-            for (int j = 0; chk && j <= i - 1; j++) {
-                if (arr[j].contains(cur)) {
-                    chk = false;
+            if (!list.contains(arr[i])) {
+                count++;
+                list.add(arr[i]);
+                for (int j = 0; j < arr[i].length(); j++) {
+                    String newStr = arr[i].substring(j, arr[i].length()) + arr[i].substring(0, j);
+                    list.add(newStr);
                 }
             }
-            
-            if (chk) {
-                count++;
-            }
         }
+
         System.out.println(count);
     }
 }
