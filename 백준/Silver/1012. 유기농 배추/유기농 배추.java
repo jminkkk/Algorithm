ce0c785989e76c3cba2a09d6d7e398ed61cb958a
Moly <102847513+jminkkk@users.noreply.github.com>
2024-01-07 21:40:09 +0900

    [Silver II] Title: 유기농 배추, Time: 164 ms, Memory: 16896 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -4,35 +4,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int dx[] = {0, 0, -1, 1};
-    static int dy[] = {-1, 1, 0, 0};
+    static int[] dx = {0, 0, -1, 1}; // 상하좌우
+    static int[] dy = {-1, 1, 0, 0}; // 상하좌우
+    static boolean[][] visited;
+    static int[][] map;
+    static int count;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < T; i++) {
-            String[] arr = br.readLine().split(" ");
-            int M = Integer.parseInt(arr[0]);
-            int N = Integer.parseInt(arr[1]);
-            int K = Integer.parseInt(arr[2]);
-
-            int[][] map = new int[N][M];
-            boolean[][] visited = new boolean[N][M];
+            count = 0;
+            String[] str = br.readLine().split(" ");
+            int M = Integer.parseInt(str[0]), N = Integer.parseInt(str[1]), K = Integer.parseInt(str[2]);
+            map = new int[N][M];
+            visited = new boolean[N][M];
 
             for (int j = 0; j < K; j++) {
-                String[] temp = br.readLine().split(" ");
-                int x = Integer.parseInt(temp[0]);
-                int y = Integer.parseInt(temp[1]);
+                String[] str2 = br.readLine().split(" ");
+                int x = Integer.parseInt(str2[0]), y = Integer.parseInt(str2[1]);
                 map[y][x] = 1;
             }
 
-            int count = 0;
             for (int j = 0; j < N; j++) {
                 for (int k = 0; k < M; k++) {
-                    if (!visited[j][k] && map[j][k] == 1) {
-                        dfs(map, visited, j, k);
+                    if (map[j][k] == 1 && !visited[j][k]) {
                         count++;
+                        dfs(j, k);
                     }
                 }
             }
@@ -41,20 +40,17 @@ public class Main {
         }
     }
 
-    public static void dfs(int[][] map, boolean[][] visited, int x, int y) {
-        if (x < 0 || x >= map.length || y < 0 || y >= map[0].length) {
-            return;
-        }
-
+    public static void dfs(int x, int y) {
         visited[x][y] = true;
 
         for (int i = 0; i < 4; i++) {
-            int nextX = x + dx[i];
-            int nextY = y + dy[i];
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-            if (nextX >= 0 && nextX < map.length && nextY >= 0 && nextY < map[0].length) {
-                if (!visited[nextX][nextY] && map[nextX][nextY] == 1) {
-                    dfs(map, visited, nextX, nextY);
+            if (nx >= 0 && nx < map.length && ny >= 0 && ny < map[0].length) {
+                if (map[nx][ny] == 1 && !visited[nx][ny]) {
+                    visited[nx][ny] = true;
+                    dfs(nx, ny);
                 }
             }
         }
