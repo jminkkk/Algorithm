28e6b0a5399b53df25f5330b3219e073466fcdc0
Moly <102847513+jminkkk@users.noreply.github.com>
2024-01-21 21:46:12 +0900

    [Silver V] Title: 집합, Time: 3684 ms, Memory: 328660 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Main {
+   
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      StringBuilder sb = new StringBuilder();
+      int m = sc.nextInt();
+      Set<Integer> set = new HashSet<>();
+      
+      for (int i = 0; i < m; i++) {
+         String cal = sc.next();
+         int x = 0;
+         
+         switch (cal) {
+            case "add" :
+               x = sc.nextInt();
+               set.add(x);
+               break;   
+            case "remove" :
+               x = sc.nextInt();
+               set.remove(x);
+               break;
+            case "check" :
+               x = sc.nextInt();
+               if (set.contains(x))
+                  sb.append("1\n");
+               else
+                  sb.append("0\n");
+               break;
+            case "toggle" :
+               x = sc.nextInt();
+               if (set.contains(x))
+                  set.remove(x);
+               else
+                  set.add(x);
+               break;   
+            case "all" :
+               for (int k = 0; k < 20; k++) {
+                  set.add(k + 1);
+               }
+               break;
+            case "empty" :
+               set.clear();
+               break;               
+         }
+      }
+
+      System.out.print(sb.toString());
+      sc.close();
+   }
+}
