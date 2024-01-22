f11466b1b92a1098deec2325e41a423591c36f2c
Moly <102847513+jminkkk@users.noreply.github.com>
2024-01-22 20:55:23 +0900

    [Silver III] Title: 빈도 정렬, Time: 188 ms, Memory: 15240 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.java
@@ -1,27 +1,35 @@
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Map<Integer, Integer> map = new LinkedHashMap<>();
-
-        Integer N = Integer.parseInt(br.readLine().split(" ")[0]);
         String[] str = br.readLine().split(" ");
-        for (int i = 0; i < N; i++) {
-            map.merge(Integer.parseInt(str[i]), 1, Integer::sum);
+        int n = Integer.parseInt(str[0]);
+        int c = Integer.parseInt(str[1]);
+        str = br.readLine().split(" ");
+
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+        for (String num : str) {
+            map.merge(Integer.parseInt(num), 1, Integer::sum);
         }
 
         map.entrySet().stream()
-            .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-            .forEach(entry -> {
-                for (int i = 0; i < entry.getValue(); i++) {
-                    System.out.print(entry.getKey() + " ");
-                }
-            });
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .forEach(entry -> {
+                    for (int i = 0; i < entry.getValue(); i++) {
+                        System.out.print(entry.getKey() + " ");
+                    }
+                });
     }
 }
