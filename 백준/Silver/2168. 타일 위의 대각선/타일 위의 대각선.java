46792a93f3236ddd15d6f0ab1108e4f328729c2b
Moly <102847513+jminkkk@users.noreply.github.com>
2024-01-29 21:37:39 +0900

    [Silver I] Title: 타일 위의 대각선, Time: 120 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Silver/2168. 타일 위의 대각선/타일 위의 대각선.java
+++ 백준/Silver/2168. 타일 위의 대각선/타일 위의 대각선.java
@@ -1,15 +1,16 @@
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] str = br.readLine().split(" ");
-        int x = Integer.parseInt(str[0]), y = Integer.parseInt(str[1]);
-        int gcd = gcd(x, y);
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
 
-        System.out.println(x + y - gcd);
+        System.out.println(n + m - gcd(n, m));
     }
 
     private static int gcd(int a, int b) {
