9429ce0e0567ed744af0e4d4777e7d27ef25aeeb
Moly <102847513+jminkkk@users.noreply.github.com>
2024-01-29 21:57:05 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 1404 ms, Memory: 39924 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,44 +1,42 @@
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         boolean[] isPrime = new boolean[1_000_000 + 1];
-
         for (int i = 2; i <= 1_000_000; i++) {
             isPrime[i] = true;
         }
 
-        // i의 배수들 제거
         for (int i = 2; i <= 1_000_000; i++) {
             for (int j = i * 2; j <= 1_000_000; j += i) {
-                if (!isPrime[j]) {
-                    continue;
-                }
                 isPrime[j] = false;
             }
         }
 
+        int n = Integer.parseInt(br.readLine());
         while (true) {
-            int number = Integer.parseInt(br.readLine());
-            if (number == 0) {
+            if (n == 0) {
                 break;
             }
 
-            boolean isDone = false;
-            for (int i = 2; i <= number / 2; i++) {
-                if (isPrime[i] && isPrime[number - i]) {
-                    System.out.println(number + " = " + i + " + " + (number - i));
-                    isDone = true;
+            boolean isGoldbach = false;
+            for (int i = 1; i <= n / 2; i++) {
+                if (isPrime[i] && isPrime[n - i]) {
+                    System.out.println(n + " = " + i + " + " + (n - i));
+                    isGoldbach = true;
                     break;
                 }
             }
 
-            if (!isDone) {
+            if (!isGoldbach) {
                 System.out.println("Goldbach's conjecture is wrong.");
             }
+
+            n = Integer.parseInt(br.readLine());
         }
     }
 }
