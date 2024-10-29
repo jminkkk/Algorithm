cbb9b17ed1927e18860ac50786eed1792e11de61
Moly <102847513+jminkkk@users.noreply.github.com>
2024-10-30 00:26:57 +0900

    [level 1] Title: 최댓값 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59415. 최댓값 구하기/최댓값 구하기.sql
+++ 프로그래머스/1/59415. 최댓값 구하기/최댓값 구하기.sql
@@ -1,5 +1,7 @@
 -- 코드를 입력하세요
-SELECT DATETIME AS 시간
-    FROM ANIMAL_INS
-    ORDER BY DATETIME DESC
-    LIMIT 1
+SELECT DATETIME
+FROM ANIMAL_INS
+ORDER BY DATETIME DESC
+LIMIT 1;
+
+--  동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부
