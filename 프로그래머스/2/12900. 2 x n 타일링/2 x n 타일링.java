32b0ba09d5120b262d03a4aaac15143ad378e2f1
Moly <102847513+jminkkk@users.noreply.github.com>
2024-11-02 13:51:13 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.54 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        if (n == 1) {
+            return 1;
+        }
+        
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+            dp[i] = dp[i] % 1000000007;
+        }
+        
+        int answer = dp[n] % 1000000007;
+        return answer;
+    }
+}
