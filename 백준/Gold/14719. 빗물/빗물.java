4dacd87fded5c0d6f80e79cc5feb142ac8214dd8
Moly <102847513+jminkkk@users.noreply.github.com>
2024-12-21 14:36:08 +0900

    [Gold V] Title: 빗물, Time: 116 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -1,42 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-    static int H;
-    static int W;
-    static Integer[] height;
+    static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] WH = br.readLine().split(" ");
-        H = Integer.parseInt(WH[0]);
-        W = Integer.parseInt(WH[1]);
-        String[] heightStr = br.readLine().split(" ");
-        height = new Integer[W];
+        String[] str = br.readLine().split(" ");
 
-        List<Integer> start = new ArrayList<>(), finish = new ArrayList<>();
-        for (int i = 0; i < W; i++) {
-            height[i] = Integer.parseInt(heightStr[i]);
+        int h = Integer.parseInt(str[0]);
+        int w = Integer.parseInt(str[1]);
+        str = br.readLine().split(" ");
+
+        arr = new int[w];
+        for (int i = 0; i < w; i++) {
+            arr[i] = Integer.parseInt(str[i]);
+        }
+
+        if (w == 1) {
+            System.out.println(0);
+            return;
         }
 
         int sum = 0;
-        for(int i = 1; i < W - 1; i++) { 
+
+        for (int i = 0; i < w; i++) {
             int left = 0;
             int right = 0;
 
-            for(int j = 0; j < i; j++) {
-                left = Math.max(height[j], left);
+            for (int j = 0; j < i; j++) {
+                left = Math.max(left, arr[j]);
             }
 
-            for(int j = i + 1; j < W; j++) {
-                right = Math.max(height[j], right);
+            for (int j = i + 1;  j < w; j++) {
+                right = Math.max(right, arr[j]);
             }
 
-            if(height[i] < left && height[i] < right)
-                sum += Math.min(left, right) - height[i];
+            if (arr[i] < left && arr[i] < right) {
+                sum += Math.min(left, right) - arr[i];
+            }
         }
 
         System.out.println(sum);
