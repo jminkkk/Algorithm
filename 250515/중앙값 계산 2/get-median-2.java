94c98125affa56d08ce6309978aef0da4618aa25
Moly <102847513+jminkkk@users.noreply.github.com>
2025-05-15 22:43:39 +0900

    Title: 중앙값 계산 2, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

--- 250515/중앙값 계산 2/get-median-2.java
+++ 250515/중앙값 계산 2/get-median-2.java
@@ -11,7 +11,7 @@ public class Main {
         
         // Please write your code here.
         for (int i = 0; i < n; i += 2) {
-            Arrays.sort(arr, 0, i);
+            Arrays.sort(arr, 0, i + 1);
             System.out.print(arr[i / 2] + " ");
         } // 1 2 3 4 5 6 7
     }
