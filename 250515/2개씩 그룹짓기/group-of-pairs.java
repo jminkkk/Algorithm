130d9323497c5ec156ebd627997e7a5c1a2e6dde
Moly <102847513+jminkkk@users.noreply.github.com>
2025-05-15 22:05:47 +0900

    Title: 2개씩 그룹짓기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250515/2개씩 그룹짓기/group-of-pairs.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[2 * n];
+        for (int i = 0; i < 2 * n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        // Please write your code here.
+        Arrays.sort(nums);
+        // 0 1 2 3 4 5
+        int max = nums[0] + nums[n - 1];
+        for (int i = 1; i < n / 2; i++) {
+            max = Math.max(min, nums[i] + nums[n - 1 - i]);
+        }
+        System.out.println(min);
+    }
+}
