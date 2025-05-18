21113f07b1bf32415951460a70101c920aabd0cc
Moly <102847513+jminkkk@users.noreply.github.com>
2025-05-18 14:07:08 +0900

    Title: Date to Date, Time: 123ms, Memory: 10MB, Status: Passed - Codetree

--- 250518/Date to Date/date-to-date.java
+++ 250518/Date to Date/date-to-date.java
@@ -1,5 +1,18 @@
 import java.util.Scanner;
 public class Main {
+    static int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    private static int find(int m, int d) {
+        int sum = 0;
+
+        for (int i = 1; i < m; i++) {
+            sum += days[i];
+        }
+
+        sum += d;
+        return sum;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m1 = sc.nextInt();
@@ -8,30 +21,7 @@ public class Main {
         int d2 = sc.nextInt();
         // Please write your code here.
         
-        int sum = 0;
-        while (true) {
-            if (m1 == m2) {
-                if  (d1 == d2) {
-                    sum += (d2 - d1) + 1;
-                } else {
-                    sum += (d2 - d1);
-                }
-                
-                break;
-            }
 
-            if (m1 == 1 || m1 == 3 || m1 == 5 || m1 == 7 || m1 == 9 || m1 == 11) {
-                sum += 31;
-                m1++;
-            } else if (m1 == 2){
-                sum += 28;
-                m1++;
-            } else {
-                sum += 30;
-                m1++;
-            }
-        }
-
-        System.out.println(sum);
+        System.out.println(find(m2, d2) - find(m1, d1) + 1);
     }
 }
