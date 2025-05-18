6c35813f7982e79dfa37f2c402389fa3332f6435
Moly <102847513+jminkkk@users.noreply.github.com>
2025-05-18 14:35:19 +0900

    Title: 요일 맞추기, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250518/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main {
+    static int[] days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 30, 31, 30, 31, 30};
+    static String[] yoil = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+    private static int get(int m, int d) {
+        int sum = 0;
+        for (int i = 1; i < m; i++) {
+            sum += days[i];
+        }
+
+        sum += d;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m1 = sc.nextInt();
+        int d1 = sc.nextInt();
+        int m2 = sc.nextInt();
+        int d2 = sc.nextInt();
+        
+        int gap = get(m2, d2) - get(m1, d1);
+
+        if (gap >= 0) {
+            System.out.println(yoil[gap % 7]);
+        } else {
+            System.out.println(yoil[7 - (Math.abs(gap) % 7)]);
+        }
+    }
+}
