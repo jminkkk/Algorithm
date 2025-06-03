9ea02eda31756a3e047f6c486848e8f04042f9c5
Moly <102847513+jminkkk@users.noreply.github.com>
2025-06-04 00:33:45 +0900

    Title: 최대로 겹치는 구간, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

--- 250604/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 250604/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -6,23 +6,18 @@ public class Main {
         int[] x1 = new int[n];
         int[] x2 = new int[n];
         
-        int[][] arr = new int[n][100];
+        int[] arr = new int[201];
         for (int i = 0; i < n; i++) {
-            x1[i] = sc.nextInt();
-            x2[i] = sc.nextInt();
-            arr[i][x1[i]] = 1;
-            arr[i][x2[i]] = -1;
+            x1[i] = sc.nextInt() + 100;
+            x2[i] = sc.nextInt() + 100;
+            for (int j = x1[i]; j < x2[i]; j++) {
+                arr[j]++;
+            }
         }
 
         int max = 0;
-        for (int i = 1; i < 100; i++) {
-            int now = 0;
-            for (int j = 0; j < n; j++) {
-                arr[j][i] += arr[j][i - 1];
-                now += arr[j][i];
-            }
-
-            max = Math.max(max, now);
+        for (int i = 0; i < 201; i++) {
+            max = Math.max(max, arr[i]);
         }
 
         System.out.println(max);
