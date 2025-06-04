988c7ea2090d9c549ede43da9d822f1f65b5345c
Moly <102847513+jminkkk@users.noreply.github.com>
2025-06-04 23:24:45 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 130ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250604/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int ax1 = sc.nextInt();
+        int ay1 = sc.nextInt();
+        int ax2 = sc.nextInt();
+        int ay2 = sc.nextInt();
+
+        int bx1 = sc.nextInt();
+        int by1 = sc.nextInt();
+        int bx2 = sc.nextInt();
+        int by2 = sc.nextInt();
+
+        int mx1 = sc.nextInt();
+        int my1 = sc.nextInt();
+        int mx2 = sc.nextInt();
+        int my2 = sc.nextInt();
+        // Please write your code here.
+        int maxAC = Math.max(ax1, ax2);
+        int maxAR = Math.max(ay1, ay2);
+
+        int maxBC = Math.max(bx1, bx2);
+        int maxBR = Math.max(by1, by2);
+
+        int maxMC = Math.max(mx1, mx2);
+        int maxMR = Math.max(my1, my2);
+
+        int maxR = Math.max(maxAR, maxBR);
+        maxR = Math.max(maxR, maxMR);
+
+        int maxC = Math.max(maxAC, maxBC);
+        maxC = Math.max(maxC, maxMC);
+
+        int[][] arr = new int[maxR + 1][maxC + 1];
+        for (int r = Math.min(ay1, ay2); r < Math.max(ay1, ay2); r++) {
+            for (int c = Math.min(ax1, ax2); c < Math.max(ax1, ax2); c++) {
+                arr[r][c] = 1;
+            }
+        }
+
+        for (int r = Math.min(by1, by2); r < Math.max(by1, by2); r++) {
+            for (int c = Math.min(bx1, bx2); c < Math.max(bx1, bx2); c++) {
+                arr[r][c] = 1;
+            }
+        }
+        for (int r = Math.min(my1, my2); r < Math.max(my1, my2); r++) {
+            for (int c = Math.min(mx1, mx2); c < Math.max(mx1, mx2); c++) {
+                arr[r][c] -= 1;
+            }
+        }
+        int cnt = 0;
+        for (int r = 0; r < maxR + 1; r++) {
+            for (int c = 0; c < maxC + 1; c++) {
+                if (arr[r][c] == 1) cnt++;
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
