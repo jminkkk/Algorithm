b2ceb60e8ac3c47371f367b5f738b0dfef69194a
Moly <102847513+jminkkk@users.noreply.github.com>
2025-06-07 13:15:47 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 135ms, Memory: 14MB, Status: Runtime Error - Codetree

--- 250607/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
+++ 250607/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.java
@@ -14,25 +14,29 @@ public class Main {
         int rect2_y2 = sc.nextInt() + 1000;
         
         int[][] arr = new int[rect1_y2 + 1][rect1_x2 + 1];
-        for (int i = rect1_y1; i <= rect1_y2; i++) {
-            for (int j = rect1_x1; j <= rect1_x2; j++) {
+        for (int i = rect1_y1; i < rect1_y2; i++) {
+            for (int j = rect1_x1; j < rect1_x2; j++) {
                 arr[i][j] = 1;
             }
         }
 
-        for (int i = rect2_y1; i <= rect2_y2; i++) {
-            for (int j = rect2_x1; j <= rect2_x2; j++) {
+        for (int i = rect2_y1; i < rect2_y2; i++) {
+            for (int j = rect2_x1; j < rect2_x2; j++) {
                 if (i <= rect1_x2 &&  j <= rect1_x2) arr[i][j] = 0;
             }
         }
         
-        int cnt = 0;
+        int minR = 2000, minC = 2000, maxR = 0, maxC = 0;
         for (int i = 0; i <= rect1_y2; i++) {
             for (int j = 0; j <= rect1_x2; j++) {
-                if (arr[i][j] == 1) cnt++;
+                if (arr[i][j] == 1) {
+                    minR = Math.min(minR, i);
+                    maxR = Math.max(maxR, i);
+                    minC = Math.min(minC, j);
+                    maxC = Math.max(maxC, j);
+                }
             }
         }
-        
-        System.out.println(cnt);
+        System.out.println((maxR - minR + 1) * (maxC - minC + 1));
     }
 }
