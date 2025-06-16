0c7d47fd0da14e4f479eb4216f6ec3ac9f1a19df
Moly <102847513+jminkkk@users.noreply.github.com>
2025-06-16 22:34:35 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 176ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 250616/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -0,0 +1,58 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int P = sc.nextInt();
+        int T = sc.nextInt();
+        int[][] shakes = new int[T][3];
+
+        boolean[] virus = new boolean[N];
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        for (int i = 0; i < T; i++) {
+            shakes[i][0] = sc.nextInt();
+            shakes[i][1] = sc.nextInt() - 1;
+            shakes[i][2] = sc.nextInt() - 1;
+
+            pq.add(new Node(shakes[i][0], shakes[i][1], shakes[i][2]));
+        }
+        
+        virus[P - 1] = true;
+        while (!pq.isEmpty()) {
+            Node node = pq.poll();
+            if (virus[node.x] || virus[node.y]) {
+                if ((node.x == P - 1) ||(node.y == P - 1)) {
+                    if (K <= 0) continue;
+                    K--;
+                }
+                virus[node.x] = true;
+                virus[node.y] = true;
+            }
+        }
+
+        String answer = "";
+        for (int i = 0; i < N; i++) {
+            if (virus[i]) answer += "1";
+            else answer += "0";
+        }
+        System.out.println(answer);
+    }
+}
+// N명 개발자, t초에  x- y, K번 악수, 
+
+class Node implements Comparable<Node> {
+    int t;
+    int x;
+    int y;
+
+    public int compareTo(Node n) {
+        return t - n.t;
+    }    
+
+    Node(int t, int x, int y) {
+        this.t = t; 
+        this.x = x;
+        this.y = y;
+    }
+}
