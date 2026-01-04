ac7525e9ee48ae9ee47ea268d816001ef05377ab
Moly <102847513+jminkkk@users.noreply.github.com>
2026-01-04 22:42:46 +0900

    [level 3] Title: 네트워크, Time: 0.30 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,6 +1,33 @@
+import java.util.*;
+
 class Solution {
+    
     public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[computers.length];
         int answer = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+            
+            answer++;
+            visited[i] = true;
+            Queue<Integer> q = new LinkedList<>();
+            q.add(i);
+            
+            while (!q.isEmpty()) {
+                int now = q.poll();
+                for (int j = i + 1; j < n; j++) {
+                    // if (j == i) continue;
+                    if (visited[j]) continue;
+                    
+                    if (computers[now][j] == 1) {
+                        q.add(j);
+                        visited[j] = true;
+                    }
+                }
+            }
+        }
+        
         return answer;
     }
 }
