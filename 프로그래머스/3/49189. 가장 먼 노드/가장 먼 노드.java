82914be91efd2e36d939aea899cda3de283edae6
Moly <102847513+jminkkk@users.noreply.github.com>
2026-01-27 23:16:24 +0900

    [level 3] Title: 가장 먼 노드, Time: 59.84 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -2,48 +2,48 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] edge) {
-        int answer = 0;
+        int[] answer = new int[n + 1]; // 1부터 거리가 i인 노드 개수 
         
-        List<Integer>[] list = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            list[i] = new ArrayList<>();
-        }
-
-        for (int i = 0; i < edge.length; i++) {
-            int a = edge[i][0] - 1;
-            int b = edge[i][1] - 1;
+        Map<Integer, List<Integer>> connected = new HashMap<>();
+        
+        for (int[] arr: edge) { // 노드들 연결 세팅
+            int a = arr[0] - 1;
+            int b = arr[1] - 1;
+            List aList = connected.getOrDefault(a, new ArrayList<>());
+            aList.add(b);
+            connected.put(a, aList);
             
-            list[a].add(b);
-            list[b].add(a);
+            List bList = connected.getOrDefault(b, new ArrayList<>());
+            bList.add(a);
+            connected.put(b, bList);
         }
         
-        int[] finalN = new int[n];
         boolean[] visited = new boolean[n];
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(0, 0));
-        visited[0] = true;
-        finalN[0] = 0;
         
-        int max = 0;
-        while(!q.isEmpty()) {
-            Node node = q.poll();
-            List<Integer> nums = list[node.num];
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(0, 1));
+        visited[0] = true;
+        answer[0]++;
+        
+        while (!q.isEmpty()) {
+            Node nd = q.poll();
+            answer[nd.depth]++;
             
-            for (int i = 0; i < nums.size(); i++) {
-                int close = nums.get(i);
-                if (visited[close]) continue;
-                q.add(new Node(close, node.depth + 1));
-                visited[close] = true;
-                finalN[close] = node.depth + 1;
-                max = Math.max(max, node.depth + 1);
+            List<Integer> list = connected.getOrDefault(nd.num, new ArrayList<>());
+            
+            for (int next: list) {
+                if (visited[next]) continue;
+                
+                q.add(new Node(next, nd.depth + 1));
+                visited[next] = true;
             }
         }
         
-        for (int i = 0; i < finalN.length; i++) {
-            if (finalN[i] == max) answer++;
+        for (int i = n; i  >= 0; i--) {
+            if (answer[i] != 0) return answer[i];
         }
         
-        return answer;
+        return 0;
     }
 }
 
