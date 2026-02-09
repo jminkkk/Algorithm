c0374a59f63d40fff56281d6cdb068b38004e99b
Moly <102847513+jminkkk@users.noreply.github.com>
2026-02-09 20:25:24 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860#) 
 
 ### 성능 요약
 
-메모리: 76.4 MB, 시간: 0.04 ms
+메모리: 81.1 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 0:0:39
+2026년 02월 09일 20:25:22
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,19 +1,23 @@
 class Solution {
-    boolean[] visited;
     public int solution(String name) {
         int answer = 0;
-        int move = name.length() - 1;
         
-        for(int i = 0; i < name.length(); i++) {
-            answer += Math.min(name.charAt(i) - 'A', 26 - (name.charAt(i) - 'A')); 
-            if (i < name.length() - 1 && name.charAt(i + 1) == 'A') {
-                int endA = i + 1;
-                while(endA < name.length() && name.charAt(endA) == 'A')
-                    endA++;
-                move = Math.min(move, i * 2 + (name.length() - endA));
-                move = Math.min(move, i + (name.length() - endA) * 2);
-            }
+        char[] arr = name.toCharArray();
+        
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != 'A') answer += Math.min(arr[i] - 'A', 26 - (arr[i] - 'A')); // 자리 A로 만들기
         }
-        return answer + move;
+        
+        int min = name.length();
+        for (int i = 0; i < arr.length; i++) { // 다음 위치 구하기
+            int next = i + 1;
+            
+            while (next < name.length() && arr[next] == 'A') next++;
+            
+            min = Math.min(min, 2 * i + name.length() - next);
+            min = Math.min(min, (name.length() - next) * 2 + i);
+        }
+        
+        return answer + min;
     }
 }
