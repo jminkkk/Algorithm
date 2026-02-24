a9e38d1497d6a4e527a53e5f269d47eb92f2ecf8
Moly <102847513+jminkkk@users.noreply.github.com>
2026-02-24 14:59:03 +0900

    [level 3] Title: 섬 연결하기, Time: 1.24 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=java) 
 
 ### 성능 요약
 
-메모리: 79.6 MB, 시간: 0.85 ms
+메모리: 72.4 MB, 시간: 1.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 03일 13:51:41
+2026년 02월 24일 14:59:00
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,55 +2,75 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] costs) {
-        int answer = 0;
-        boolean[] visited = new boolean[n];
-        int[] parent = new int[n];
-        
-        List<List<Node>> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-            list.add(new ArrayList<>());
-        }
-        
-        for (int i = 0; i < costs.length; i++) {
-            list.get(costs[i][0]).add(new Node(costs[i][1], costs[i][2]));
-            list.get(costs[i][1]).add(new Node(costs[i][0], costs[i][2]));
-        }
-
+        boolean[][] connect = new boolean[n][n];
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(0, 0));
         
-        while(!pq.isEmpty()) {
-            Node node = pq.poll();
-            if (visited[node.start]) continue;
+        int answer = 0;
+        
+        for (int[] arr: costs) {
+            connect[arr[0]][arr[1]] = true;
+            connect[arr[1]][arr[0]] = true;
+            answer += arr[2];
             
-            visited[node.start] = true;
-            answer += node.weight;
-            
-            for (int i = 0; i < list.get(node.start).size(); i++) {
-                int next = list.get(node.start).get(i).start;
-                int cost = list.get(node.start).get(i).weight;
-                
-                if (visited[next]) continue;
-                pq.add(new Node(next, cost));
-            }
+            pq.add(new Node(arr[0], arr[1], arr[2]));
+        }
+        
+        while (!pq.isEmpty()) {
+            Node nd = pq.poll();
 
+            connect[nd.nd1][nd.nd2] = false;
+            connect[nd.nd2][nd.nd1] = false;
+
+            if (canRemove(nd, connect)) answer -= nd.cost; // 제거해도 된다면 제거
+            else { // 제거 불가능이면 원복
+                // System.out.println(nd.nd1 + " " + nd.nd2 + " " + nd.cost);
+                connect[nd.nd1][nd.nd2] = true;
+                connect[nd.nd2][nd.nd1] = true;    
+            }            
         }
         
         return answer;
     }
+    
+    public boolean canRemove(Node nd, boolean[][] connect) {
+        int cnt = 0;
+        boolean[] visited = new boolean[connect.length];
+        
+        for (int i = 0; i < connect.length; i++) {
+            if (visited[i]) continue;
+            
+            cnt++;
+            visited[i] = true;
+            dfs(i, connect, visited);
+        }
+        
+        return cnt == 1; // 덩어리 몇개인지 파악
+    }
+    
+    public void dfs(int cur, boolean[][] connect, boolean[] visited) {        
+        for (int i = 0; i < connect.length; i++) {
+            if (i == cur) continue; // 자기 자신이랑 연결 불가능
+            
+            if (!connect[cur][i] || visited[i]) continue; // 연결 안되어있는 노드거나 이미 방문면 무시
+            
+            visited[i] = true;
+            dfs(i, connect, visited);
+        }
+    }
 }
 
 class Node implements Comparable<Node> {
-    int start;
-    int weight;
+    int cost;
+    int nd1;
+    int nd2;
     
-    public Node (int start, int weight) {
-        this.start = start;
-        this.weight = weight;
+    Node (int nd1, int nd2, int cost) {
+        this.cost = cost;
+        this.nd1 = nd1;
+        this.nd2 = nd2;
     }
     
-    public int compareTo(Node c) {
-        return this.weight - c.weight;
+    public int compareTo(Node nd) {
+        return nd.cost - this.cost;
     }
 }
