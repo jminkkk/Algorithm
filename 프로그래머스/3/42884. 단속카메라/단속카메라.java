92f4dc6998592565b66664362884c3fdd5e61f5e
Moly <102847513+jminkkk@users.noreply.github.com>
2026-02-25 11:03:45 +0900

    [level 3] Title: 단속카메라, Time: 8.99 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,17 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        Arrays.sort(routes, (int a[], int b[]) -> a[1] - b[1]);
-        int lastCamera = routes[0][1];
         int answer = 1;
         
+        Arrays.sort(routes, (o1, o2) -> { return o1[1] - o2[1]; });
+        
+        int idx = 0; // 현재 카메라 둔 위치의 경로 인덱스
         for (int i = 1; i < routes.length; i++) {
-            if (routes[i][0] > lastCamera) {
-                answer++;
-                lastCamera = routes[i][1];
-            }
+            if (routes[i][0] <= routes[idx][1]) continue;
+            
+            answer++;
+            idx = i;
         }
-
+        
         return answer;
     }
 }
