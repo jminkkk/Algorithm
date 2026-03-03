5e342bb3a5cff8fb2d2f5eaff72b6ce73c9e899c
Moly <102847513+jminkkk@users.noreply.github.com>
2026-03-03 13:59:08 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,48 +1,88 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[][] visited;
-    static int[][] graph;
-    static Set<Integer> set;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int nodes = Integer.parseInt(br.readLine());
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
 
-        graph = new int[N][N];
-        visited = new boolean[N][N];
-        set = new HashSet<>();
-
-        for (int i = 0; i < nodes; i++) {
-            String[] arr = br.readLine().split(" ");
-            int a = Integer.parseInt(arr[0]);
-            int b = Integer.parseInt(arr[1]);
-            graph[a - 1][b - 1] = 1;
-            graph[b - 1][a - 1] = 1;
-        }
-
-        dfs(0);
-
-        if (set.size() == 0) {
-            System.out.println(0);
-            return;
-        }
-
-        System.out.println(set.size() - 1);
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
     }
 
-    public static void dfs(int start) {
-        for (int i = 0; i < graph.length; i++) {
-            if (graph[start][i] == 1 && !visited[start][i]) {
-                visited[start][i] = true;
-                set.add(i);
-                set.add(start);
-                dfs(i);
-            }
+    public void solution() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int nd = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        Map<Integer, List<Integer>> con = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            String[] arr = br.readLine().split(" ");
+            int e = Integer.parseInt(arr[0]) - 1;
+            int s = Integer.parseInt(arr[1]) - 1;
+
+            List<Integer> list = con.getOrDefault(e, new ArrayList<>());
+            list.add(s);
+            con.put(e, list);
+
+            list = con.getOrDefault(s, new ArrayList<>());
+            list.add(e);
+            con.put(s, list);
+        }
+
+        boolean[] visited = new boolean[nd];
+
+        dfs(0, visited, con);
+
+        int cnt = 0;
+        for (boolean b: visited) {
+            if (b) cnt++;
+        }
+
+        System.out.println(cnt - 1);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public void dfs(int now, boolean[] visited, Map<Integer, List<Integer>> con) {
+        visited[now] = true;
+        List<Integer> list = con.getOrDefault(now, new ArrayList<>());
+
+        for (int num: list) {
+            if (visited[num]) continue;
+            dfs(num, visited, con);
         }
     }
 }
+//
+//boolean[] visited = new boolean[];
+//Map<Integer, List<Integer>> con = new HashMap<>();
+//
+//for (int i: con.get(0)) {
+//		if (visited[0][i]) continue;
+//		if (!arr[0][i]) continue;
+//
+//		dfs(i, visited);
+//	}
+//}
+//
+//dfs(int now, boolean[] visited) {
+//	visited[now] = true;
+//	List<Integer> list = con.get(now);
+//
+//	for (int num: list) {
+//		if (visited[num]) continue;
+//		dfs(num, visited);
+//	}
+//}
