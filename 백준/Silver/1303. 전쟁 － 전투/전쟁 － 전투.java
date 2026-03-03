3e3ed159fb22c8620638ba04eeac18fbd778ea53
Moly <102847513+jminkkk@users.noreply.github.com>
2026-03-03 13:15:49 +0900

    [Silver I] Title: 전쟁 - 전투, Time: 128 ms, Memory: 16104 KB -BaekjoonHub

--- 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
+++ 백준/Silver/1303. 전쟁 － 전투/전쟁 － 전투.java
@@ -1,70 +1,115 @@
-import java.util.Scanner;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
-    static char[][] map;
-    static boolean[][] visit;
-    static int n;
-    static int m;
-    static int dx[] = {-1, 1, 0, 0};
-    static int dy[] = {0, 0, -1, 1};
-    static int ally;
-    static int enemy;
-    static int allyCount;
-    static int enemyCount;
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static boolean[][] visited;
+    static char[][] arr;
+    static int sum;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
-        m = sc.nextInt();
-        n = sc.nextInt();
-        sc.nextLine();
-
-        map = new char[n][m];
-        visit = new boolean[n][m];
-        ally = 0;
-        enemy = 0;
-        allyCount = 0;
-        enemyCount = 0;
-
-        for (int i = 0; i < n; i++) {
-            String str = sc.nextLine();
-            for (int j = 0; j < str.length(); j++) {
-                map[i][j] = str.charAt(j);
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (!visit[i][j]) {
-                    dfs(i, j, map[i][j]);
-                    ally += allyCount * allyCount;
-                    enemy += enemyCount * enemyCount;
-
-                    allyCount = 0;
-                    enemyCount = 0;
-                }
-            }
-        }
-        System.out.println(ally + " " + enemy);
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
     }
 
-    private static void dfs(int x, int y, char t) {
-        visit[x][y] = true;
+    public void solution() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        if (t == 'W')
-            allyCount++;
-        else
-            enemyCount++;
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]);
+        int m = Integer.parseInt(s[1]);
 
-        for (int i = 0; i < 4; i++) {
-            int nowX = x + dx[i];
-            int nowY = y + dy[i];
+        visited =  new boolean[m][n];
+        arr = new char[m][n];
+        for (int i = 0; i < m; i++) {
+            arr[i] = br.readLine().toCharArray();
+        }
 
-            if (nowX >= 0 && nowY >= 0 && nowX < n && nowY < m) {
-                if (!visit[nowX][nowY] && map[nowX][nowY] == t) {
-                    dfs(nowX, nowY, t);
-                }
+        int bSum = 0;
+        int wSum = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (visited[i][j]) continue;
+                int sum = dfs(i, j);
+
+                if (arr[i][j] == 'B') bSum += sum * sum;
+                else  if (arr[i][j] == 'W') wSum += sum * sum;
             }
         }
+
+        System.out.println(wSum + " " + bSum);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public int dfs(int x, int y) {
+        sum = 0;
+        recursion(x, y);
+        return sum;
+    }
+
+    public void recursion(int x, int y) {
+        visited[x][y] = true;
+        sum++;
+
+        for (int d = 0; d < 4; d++) {
+            int nx =  x + dx[d];
+            int ny = y + dy[d];
+            if (canGo(nx, ny, arr[x][y]))  recursion(nx, ny);
+        }
+    }
+
+    public boolean canGo(int x, int y, char ch) {
+        return x >= 0 && y >= 0 && x < arr.length && y < arr[0].length
+                && arr[x][y] == ch && !visited[x][y];
     }
 }
+//
+//int wSum = 0;
+//int bSum = 0;
+//
+//for (int i = 0; i < n; i++) {
+//	for (int j = 0; j < m; j++) {
+//		if (visited[i][j]) continue;
+//
+//		int count = dfs(i, j, visited);
+//
+//		if (arr[i][j] == 'W') wSum += count;
+//		else bSum += count;
+//	}
+//}
+//
+//
+//dfs(int x, int y, boolean[][] visited)
+//	int cnt = 0;
+//	return recursive(x, y, 1, visited);
+//
+//recursive(int x, int y, int cnt, boolean[][] visited) {
+//	visited[x][y] = true;
+//
+//	for (int i = 0; i < 4; i++) {
+//		int nx = x + dx[i];
+//		int ny = y + dy[i];
+//
+//		if (!canGo(nx, ny, visited, arr[x][y])) continue;
+//		recursive(nx, ny, cnt + 1, visited);
+//	}
+//
+//	return cnt;
+//}
+//
+//canGo(int x, int y, boolean[][] visited, char c)
+//	return x >= 0 && x < m && y >= 0 && y < n
+//		&& !visited[x][y] && arr[x][y] == c
