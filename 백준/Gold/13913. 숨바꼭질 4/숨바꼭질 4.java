0e046a76582646fcd29d01c5b389d6d6e79f4b2a
Moly <102847513+jminkkk@users.noreply.github.com>
2026-03-13 14:57:01 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 876 ms, Memory: 44792 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.java
@@ -1,62 +1,77 @@
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-
-    static final int MAX = 100000;
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
 
     public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public void solution() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        //br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/java/BOJ_13913/input.txt")));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        String[] str = br.readLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int k = Integer.parseInt(str[1]);
 
-        boolean[] visited = new boolean[MAX + 1];
-        int[] dist = new int[MAX + 1];
-        int[] parent = new int[MAX + 1];
-
+        int[] position = new int[100_001]; // 각 위치까지 걸리는 최단 시간
+        int[] parent = new int[100_001]; // 각 위치까지 걸리는 최단 시간
+        boolean[] visited = new boolean[100_001];
         Queue<Integer> q = new LinkedList<>();
-        q.add(n);
+        position[n] = 0;
         visited[n] = true;
+        q.add(n);
 
         while (!q.isEmpty()) {
             int now = q.poll();
 
-            if (now == k) break;
+            if (now == k) {
+                break;
+            }
 
-            int[] nexts = {now - 1, now + 1, now * 2};
+            int[] nexts = new int[]{now + 1, now - 1, now * 2};
+            for (int next: nexts) {
+                if (next < 0 || next > 100_000) continue;
+                if (visited[next]) continue;
 
-            for (int next : nexts) {
-
-                if (next < 0 || next > MAX) continue;
-
-                if (!visited[next]) {
-                    visited[next] = true;
-                    dist[next] = dist[now] + 1;
-                    parent[next] = now;
-
-                    q.add(next);
-                }
+                visited[next] = true;
+                parent[next] = now;
+                position[next] = position[now] + 1;
+                q.add(next);
             }
         }
 
-        System.out.println(dist[k]);
+        System.out.println(position[k]);
 
-        List<Integer> path = new ArrayList<>();
-        int cur = k;
-
-        while (cur != n) {
-            path.add(cur);
-            cur = parent[cur];
+        int now = k;
+        List<Integer> nums = new ArrayList<>();
+        while (now != n) {
+            nums.add(now);
+            now = parent[now];
         }
-        path.add(n);
+        nums.add(n);
 
-        Collections.reverse(path);
-
-        for (int x : path) {
-            System.out.print(x + " ");
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            System.out.print(nums.get(i) + " ");
         }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
