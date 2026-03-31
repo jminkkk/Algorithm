f164a6946c04b78f359e559b3c423593f7424768
Moly <102847513+jminkkk@users.noreply.github.com>
2026-03-31 13:11:25 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 716 ms, Memory: 82832 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,102 +1,94 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    private static int N, E, Ans;
-    private static int INF = 200000000;
-    private static ArrayList<Node>[] adjList;
-    private static int[] cost;
-    private static boolean[] visited;
-    
-    private static class Node implements Comparable<Node>{
-        int dest, cost;
-        public Node(int dest, int cost){
-            this.dest = dest;
-            this.cost = cost;
-        }
-        @Override
-        public int compareTo(Node o){
-            return this.cost - o.cost;
-        }
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
     }
-    
-    public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
-        Ans = 0;
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
+    public void solution() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        //br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/java/BOJ_1504/input.txt")));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        adjList = new ArrayList[N+1];
-        cost = new int[N+1];
-        visited = new boolean[N+1];
+        String[] str = br.readLine().split(" ");
+        int n = Integer.parseInt(str[0]);
+        int e = Integer.parseInt(str[1]);
 
-        for (int i = 0; i < N+1; i++) {
-            adjList[i] = new ArrayList<>();
+        List<Node>[] graph = new ArrayList[n + 1];// 인접리스트
+        for (int i = 1; i <= n; i++) { // 1_000
+            graph[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < E; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken()); 
-            int b = Integer.parseInt(st.nextToken()); 
-            int c = Integer.parseInt(st.nextToken()); 
-            adjList[a].add(new Node(b, c));
-            adjList[b].add(new Node(a, c));
+        for (int i = 1; i <= e; i++) {
+            str = br.readLine().split(" ");
+            int bus1 = Integer.parseInt(str[0]);
+            int bus2 = Integer.parseInt(str[1]);
+            int cost = Integer.parseInt(str[2]);
+            graph[bus1].add(new Node(bus2, cost));
+            graph[bus2].add(new Node(bus1, cost));
         }
-        st = new StringTokenizer(br.readLine());
-        int v1 = Integer.parseInt(st.nextToken());
-        int v2 = Integer.parseInt(st.nextToken());
 
-        int ans1 = 0; // 1 > v1 > v2 > N
-        ans1 += dijkstra(1, v1);
-        ans1 += dijkstra(v1, v2);
-        ans1 += dijkstra(v2, N);
+        str = br.readLine().split(" ");
+        int v1 = Integer.parseInt(str[0]);
+        int v2 = Integer.parseInt(str[1]);
 
-        int ans2 = 0; // 1 > v2 > v1 > N
-        ans2 += dijkstra(1, v2);
-        ans2 += dijkstra(v2, v1);
-        ans2 += dijkstra(v1, N);
+        long firstPath = (long) dijkstra(1, v1, graph) + dijkstra(v1, v2, graph) + dijkstra(v2, n, graph);
+        long secondPath = (long) dijkstra(1, v2, graph) + dijkstra(v2, v1, graph) + dijkstra(v1, n, graph);
 
-        if(ans1 >= INF && ans2 >= INF) Ans = -1;
-        else Ans = Math.min(ans1, ans2); 
+        long min = Math.min(firstPath, secondPath);
 
-        bw.write(Ans+"\n");
+        if (min >= Integer.MAX_VALUE) min = -1;
+        System.out.println(min);
+
         bw.flush();
         bw.close();
         br.close();
     }
-    private static int dijkstra(int start, int end){
+
+    public static int dijkstra(int start, int end, List<Node>[] graph) {
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        Arrays.fill(visited, false); 
-        Arrays.fill(cost, INF);
+        int[] dist = new int[graph.length];
+        for (int i = 1; i < graph.length; i++) { // 1_000
+            dist[i] = Integer.MAX_VALUE;
+        }
 
-        cost[start] = 0; 
         pq.add(new Node(start, 0));
+        dist[start] = 0;
 
-        while (!pq.isEmpty()){
+        while (!pq.isEmpty()) { // 100_000
             Node now = pq.poll();
 
-            if(visited[now.dest]) continue; 
-            visited[now.dest] = true; 
+            if (now.cost > dist[now.pos]) continue;
 
-            for (Node next:adjList[now.dest]) {
-                if(cost[next.dest] > next.cost + now.cost){
-                    cost[next.dest] = next.cost + now.cost;
-                    pq.add(new Node(next.dest, cost[next.dest]));
-                }
+            for (Node next: graph[now.pos]) {
+                int nextCost = now.cost + next.cost;
+                if (nextCost >= dist[next.pos]) continue;
+                pq.add(new Node(next.pos, nextCost));
+                dist[next.pos] = nextCost;
             }
         }
 
-        return cost[end]; 
+        return dist[end];
+    }
+}
+
+class Node implements Comparable<Node> {
+    int pos;
+    int cost;
+
+    public Node(int pos, int cost) {
+        this.pos = pos;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.cost - o.cost;
     }
 }
 
