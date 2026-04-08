9787cdb678f076959de61b1daec8d807588538c7
Moly <102847513+jminkkk@users.noreply.github.com>
2026-04-08 11:34:42 +0900

    [Gold IV] Title: 네트워크 연결, Time: 548 ms, Memory: 62468 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,6 +1,15 @@
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
     static BufferedReader br;
@@ -11,67 +20,54 @@ public class Main {
         new Main().solution();
     }
 
-    public static int find(int x, int[] parent) {
-        if (parent[x] == x) return x;
-        parent[x] = find(parent[x], parent);
-        return parent[x];
-    }
-
-    public static boolean union(int a, int b, int[] parent) {
-        int rootA = find(a, parent);
-        int rootB = find(b, parent);
-
-        if (rootA == rootB) return false;
-        parent[rootA] = rootB;
-        return true;
-    }
-
     public void solution() throws Exception {
         br = new BufferedReader(new InputStreamReader(System.in));
-        //br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/java//input.txt")));
         bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        // 크루스칼
 
         String[] str = br.readLine().split(" ");
         int n = Integer.parseInt(str[0]);
         str = br.readLine().split(" ");
         int m = Integer.parseInt(str[0]);
 
-//        List<int[]>[] graph = new ArrayList[n + 1];
-        List<int[]> kruskalGraph = new ArrayList<>();
-//        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+        boolean[] visited = new boolean[n + 1];
+        List<int[]>[] edges = new ArrayList[n + 1];
 
-        for (int i = 0; i < m; i++) {
+        for (int i = 1; i <= n; i++) {
+            edges[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= m; i++) {
             str = br.readLine().split(" ");
             int a = Integer.parseInt(str[0]);
             int b = Integer.parseInt(str[1]);
             int c = Integer.parseInt(str[2]);
 
-            kruskalGraph.add(new int[]{a, b, c});
-//            graph[a].add(new int[]{b, c});
-//            graph[b].add(new int[]{a, c});
+            edges[a].add(new int[]{b, c});
+            edges[b].add(new int[]{a, c});
         }
 
-        kruskalGraph.sort((a, b) -> a[2] - b[2]);
-        int[] parents = new int[n + 1];
-        for (int i = 1; i <= n; i++) parents[i] = i;
-        int sum = 0;
+        Set<Integer> mst = new HashSet<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        pq.add(new int[]{1, 0});
 
-        List<int[]> mst = new ArrayList<>();
-        for (int[] edge: kruskalGraph) {
-            int a = edge[0];
-            int b = edge[1];
-            int cost = edge[2];
+        int cost = 0;
+        while(!pq.isEmpty()) {
+            int[] now = pq.poll();
 
-            if (!union(a, b, parents)) continue;
+            if (visited[now[0]]) continue;
+            cost += now[1];
+            mst.add(now[0]);
+            visited[now[0]] = true;
+            
+            if (mst.size() == n) break;
 
-            mst.add(new int[]{a, b, cost});
-            sum += cost;
-            if (mst.size() == n - 1) break;
+            for (int[] next: edges[now[0]]) {
+                if (visited[next[0]]) continue;
+                pq.add(next);
+            }
         }
 
-        System.out.println(sum);
+        System.out.println(cost);
 
         bw.flush();
         bw.close();
