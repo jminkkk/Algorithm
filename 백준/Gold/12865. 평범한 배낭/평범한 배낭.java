e2837aebe8ec9d917e9b49deb7d0fc8ed7a6c412
Moly <102847513+jminkkk@users.noreply.github.com>
2026-04-28 14:16:38 +0900

    [Gold V] Title: 평범한 배낭, Time: 176 ms, Memory: 54092 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,23 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
+public class Main {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static StringTokenizer st;
 
-class Main {
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
-    static int max = 0;
-    static int n, m;
-    static int[][] dp;
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
+    }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public void solution() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        //br = new BufferedReader(new InputStreamReader(new FileInputStream("src/main/java/BOJ_12865/input.txt")));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         String[] str = br.readLine().split(" ");
-        n = Integer.parseInt(str[0]);
-        m = Integer.parseInt(str[1]);
+        int n = Integer.parseInt(str[0]);
+        int k = Integer.parseInt(str[1]);
 
-        dp = new int[n + 1][m + 1];
         int[][] arr = new int[n + 1][2];
         for (int i = 1; i <= n; i++) {
             str = br.readLine().split(" ");
@@ -25,18 +26,25 @@ class Main {
             arr[i][1] = Integer.parseInt(str[1]);
         }
 
-        Arrays.fill(arr[0], 0);
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 0; j < m + 1; j++) {
-                // dp[i][w] 최대 무게가 w일 때, i번째 가방까지 포함한 최대 가치
-                int w = arr[i][0];
-                if (w > j) dp[i][j] = dp[i - 1][j]; // 지금꺼는 못 넣어
-                else {
-                    int v = arr[i][1];
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + v);
+        int[][] dp = new int[n + 1][k + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                int curW = arr[i][0];
+                if (j < curW) { // 가능한 무게보다 클 때
+                    dp[i][j] = dp[i - 1][j];
+                    continue;
                 }
+
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - curW] + arr[i][1]);
             }
         }
-        System.out.println(dp[n][m]);
+
+        System.out.println(dp[n][k]);
+
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
