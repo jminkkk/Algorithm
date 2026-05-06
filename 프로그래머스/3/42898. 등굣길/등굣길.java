26d65f988759a6a642b41caa3cacd1193f3eaf43
Moly <102847513+jminkkk@users.noreply.github.com>
2026-05-06 17:23:11 +0900

    [level 3] Title: 등굣길, Time: 0.74 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[n + 1][m + 1];
+        boolean[][] cantGo = new boolean[n + 1][m + 1];
+        
+        for (int[] puddle: puddles) {
+	        int r = puddle[1];
+	        int c = puddle[0];
+	        cantGo[r][c] = true;
+	    }
+	      
+        dp[1][1] = 1;
+        
+	    for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 && j == 1) continue;
+                if (cantGo[i][j]) continue;
+
+			    if (cantGo[i - 1][j] && cantGo[i][j - 1]) {
+				    cantGo[i][j] = true;
+				    continue;
+				}
+				dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1_000_000_007;
+		      }
+	      }
+        
+        return dp[n][m];
+    }
+}
