161eceb99e589ff66ccd4ad48425b7014e625cf1
Moly <102847513+jminkkk@users.noreply.github.com>
2026-07-06 15:34:21 +0900

    [level 3] Title: 단어 변환, Time: 0.39 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,75 +1,43 @@
 import java.util.*;
 
 class Solution {
-    int answer = Integer.MAX_VALUE;
     
     public int solution(String begin, String target, String[] words) {
+        int answer = 0;
         int strLen = begin.length();
         int wordsLen = words.length;
-        boolean[][] arr = new boolean[wordsLen][wordsLen];
         
-        // 주어진 단어 간 변환 가능한지 마킹 
-        for (int i = 0; i < wordsLen; i++) {
-            for (int j = 0; j < wordsLen; j++) {
-                if (i == j) continue;
-                
-                int diff = 0;
-                for (int k = 0; k < strLen; k++) {
-                    if (words[i].charAt(k) != words[j].charAt(k)) diff++;
-                }
-                
-                if (diff == 1) {
-                    arr[i][j] = true;
-                }
-            }
-        }
-        
-        // 시작 단어에서 변환 가능한지
-        for (int i = 0; i < wordsLen; i++) {
-            int diff = 0;
-            for (int j = 0; j < strLen; j++) {
-                if (words[i].charAt(j) != begin.charAt(j)) diff++;
-            }
-                
-            if (diff == 1) bfs(words, arr, target, i);
-        }
-        
-        answer = answer == Integer.MAX_VALUE ? 0 : answer;
-        return answer;
-    }
-    
-    private void bfs(String[] words, boolean[][] arr, String target, int idx) {
-        Queue<Node> q = new LinkedList<>();
-        boolean[] visited = new boolean[words.length];
-        q.add(new Node(1, idx));
-        visited[idx] = true;
+        Queue<String> q = new LinkedList<>();
+        boolean[] visited = new boolean[wordsLen];
+        q.add(begin);
         
         while (!q.isEmpty()) {
-            Node nd = q.poll();
-            
-            if (target.equals(words[nd.idx])) {
-                answer = Math.min(answer, nd.depth);
-                return;
+            int size = q.size();
+                        
+            for (int i = 0; i < size; i++) {
+                String str = q.poll();
+                if (target.equals(str)) {
+                    return answer;
+                }
+                
+                for (int j = 0; j < wordsLen; j++) {
+                    if (visited[j]) continue;
+                    int diff = 0;
+                    
+                    for (int k = 0; k < strLen; k++) {
+                        if (words[j].charAt(k) != str.charAt(k)) diff++;
+                    }
+
+                    if (diff != 1) continue;
+
+                    q.add(words[j]);
+                    visited[j] = true;
+                }
             }
             
-            for (int i = 0; i < arr.length; i++) {
-                if (visited[i]) continue;
-                if (!arr[nd.idx][i]) continue;
-                q.add(new Node(nd.depth + 1, i));
-                visited[i] = true;
-            }
+            answer++;
         }
         
-    }
-}
-
-
-class Node {
-    int depth;
-    int idx;
-    
-    Node(int depth, int idx) {
-        this.depth = depth;
-        this.idx = idx;
+        return 0;
     }
 }
