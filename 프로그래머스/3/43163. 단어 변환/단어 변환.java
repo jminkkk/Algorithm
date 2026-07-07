778b99e864f2f0bbf155e374c5fb4890046d2bd4
Moly <102847513+jminkkk@users.noreply.github.com>
2026-07-07 14:53:02 +0900

    [level 3] Title: 단어 변환, Time: 0.36 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,43 +1,50 @@
 import java.util.*;
 
 class Solution {
-    
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        int strLen = begin.length();
-        int wordsLen = words.length;
+        int answer = 1;
+        int len = begin.length();
+        boolean[] visited = new boolean[words.length];
         
         Queue<String> q = new LinkedList<>();
-        boolean[] visited = new boolean[wordsLen];
-        q.add(begin);
+        for (int i = 0; i < words.length; i++) {
+            if (validateDiff(len, words[i], begin)) {
+                visited[i] = true;
+                q.add(words[i]);
+            }
+        }
+
+        if (q.size() == 0) return 0;
         
         while (!q.isEmpty()) {
             int size = q.size();
-                        
+            if (q.contains(target)) return answer;            
+            
             for (int i = 0; i < size; i++) {
-                String str = q.poll();
-                if (target.equals(str)) {
-                    return answer;
-                }
+                String now = q.poll();
                 
-                for (int j = 0; j < wordsLen; j++) {
+                for (int j = 0; j < words.length; j++) {
                     if (visited[j]) continue;
-                    int diff = 0;
+                    if (!validateDiff(len, now, words[j])) continue;
                     
-                    for (int k = 0; k < strLen; k++) {
-                        if (words[j].charAt(k) != str.charAt(k)) diff++;
-                    }
-
-                    if (diff != 1) continue;
-
-                    q.add(words[j]);
                     visited[j] = true;
+                    q.add(words[j]);
                 }
             }
             
             answer++;
+            
+        }
+
+        return 0;
+    }
+    
+    private boolean validateDiff(int len, String a, String b) {
+        int diff = 0;
+        for (int i = 0; i < len; i++) {
+            if (a.charAt(i) != b.charAt(i)) diff++;
         }
         
-        return 0;
+        return diff == 1;
     }
 }
