2ba3c29b890c82d56551998fe987d3d3e34b0471
Moly <102847513+jminkkk@users.noreply.github.com>
2026-07-12 14:26:10 +0900

    [level 3] Title: 섬 연결하기, Time: 1.05 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 1.24 ms
+메모리: 76.3 MB, 시간: 1.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 24일 14:59:00
+2026년 07월 12일 14:26:04
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,75 +2,42 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] costs) {
-        boolean[][] connect = new boolean[n][n];
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        
         int answer = 0;
         
-        for (int[] arr: costs) {
-            connect[arr[0]][arr[1]] = true;
-            connect[arr[1]][arr[0]] = true;
-            answer += arr[2];
-            
-            pq.add(new Node(arr[0], arr[1], arr[2]));
-        }
+        List<int[]>[] graph = new ArrayList[n];
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
         
+        for (int[] c: costs) {
+            int from = c[0];
+            int to = c[1];
+            int cost = c[2];
+            graph[from].add(new int[]{to, cost});
+            graph[to].add(new int[]{from, cost});
+        }        
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> Integer.compare(a[1], b[1]));
+        boolean[] visited = new boolean[n];
+        pq.offer(new int[]{0, 0});
+            
         while (!pq.isEmpty()) {
-            Node nd = pq.poll();
-
-            connect[nd.nd1][nd.nd2] = false;
-            connect[nd.nd2][nd.nd1] = false;
-
-            if (canRemove(nd, connect)) answer -= nd.cost; // 제거해도 된다면 제거
-            else { // 제거 불가능이면 원복
-                // System.out.println(nd.nd1 + " " + nd.nd2 + " " + nd.cost);
-                connect[nd.nd1][nd.nd2] = true;
-                connect[nd.nd2][nd.nd1] = true;    
-            }            
+            int[] now = pq.poll();
+            int node = now[0];
+            int cost = now[1];
+            
+            if (visited[node]) continue;
+            visited[node] = true;
+            answer += cost;
+            
+            List<int[]> nexts = graph[node];
+            for (int[] nx: nexts) {
+                if (!visited[nx[0]]) pq.add(nx);
+            }
         }
         
         return answer;
     }
-    
-    public boolean canRemove(Node nd, boolean[][] connect) {
-        int cnt = 0;
-        boolean[] visited = new boolean[connect.length];
-        
-        for (int i = 0; i < connect.length; i++) {
-            if (visited[i]) continue;
-            
-            cnt++;
-            visited[i] = true;
-            dfs(i, connect, visited);
-        }
-        
-        return cnt == 1; // 덩어리 몇개인지 파악
-    }
-    
-    public void dfs(int cur, boolean[][] connect, boolean[] visited) {        
-        for (int i = 0; i < connect.length; i++) {
-            if (i == cur) continue; // 자기 자신이랑 연결 불가능
-            
-            if (!connect[cur][i] || visited[i]) continue; // 연결 안되어있는 노드거나 이미 방문면 무시
-            
-            visited[i] = true;
-            dfs(i, connect, visited);
-        }
-    }
 }
 
-class Node implements Comparable<Node> {
-    int cost;
-    int nd1;
-    int nd2;
-    
-    Node (int nd1, int nd2, int cost) {
-        this.cost = cost;
-        this.nd1 = nd1;
-        this.nd2 = nd2;
-    }
-    
-    public int compareTo(Node nd) {
-        return nd.cost - this.cost;
-    }
-}
+// MST
+// 크루스칼
+// 프림
