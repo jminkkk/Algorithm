61eb654432d757c44b2fbc46cdfcd8f0b71a9e55
Moly <102847513+jminkkk@users.noreply.github.com>
2026-08-06 17:35:27 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 12.19 ms, Memory: 57.7 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -1,19 +1,23 @@
 class Solution {
     public int solution(String s) {
-        for (int len = s.length(); len > 0; len--) {
-            for (int str = 0; str <= s.length() - len; str++) {
-                boolean allPass = true;
-                
-                for (int i = 0; i < len / 2; i++) {
-                    if (s.charAt(i + str) == s.charAt(len + str - i - 1)) continue;
-                    allPass = false;
-                    break;
-                }
-                
-                if (allPass) return len;
-            }
-        }
+        int answer = 1;
 
-        return 0;
+        for (int ctr = 0; ctr < s.length() - 1; ctr++) {
+            answer = Math.max(answer, checkLen(s, ctr, ctr));    
+            answer = Math.max(answer, checkLen(s, ctr, ctr + 1));
+        }
+        
+        return answer;
+    }
+
+    private int checkLen(String s, int leftIdx, int rightIdx) {
+        while (leftIdx >= 0
+              && rightIdx < s.length()
+              && s.charAt(leftIdx) == s.charAt(rightIdx)) {
+            leftIdx--;
+            rightIdx++;
+        }
+        
+        return rightIdx - leftIdx - 1;
     }
 }
